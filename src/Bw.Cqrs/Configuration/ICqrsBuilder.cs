986b0d641bfb98f03f3055fb9d28d0ca3ddc9f0d
meysams <soleymani.meysam.developer@gmail.com>
2025-03-27 18:43:25 +0330

    feat: add event handling DI registration with proper type scanning

--- src/Bw.Cqrs/Configuration/ICqrsBuilder.cs
+++ src/Bw.Cqrs/Configuration/ICqrsBuilder.cs
@@ -1,8 +1,10 @@
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Bw.Cqrs.Configuration;
 
 public interface ICqrsBuilder
 {
     IServiceCollection Services { get; }
+    IEnumerable<Assembly> Assemblies { get; }
 } 
